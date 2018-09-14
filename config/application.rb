a90896072734001e559ee9a3ea51e3612f538fe3
EC2 Default User <ec2-user@ip-172-31-29-84.us-east-2.compute.internal>
2018-09-14 14:07:28 +0000

    s3sakujo

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,6 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
-
 module Joshikuru
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
